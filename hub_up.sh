7a8ae39b0edd62d7c5b4b3757fa475ab19147a15
Carl <cboettig@gmail.com>
2024-09-30 20:49:11 -0700

    hub config

---
+++ hub_up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+helm repo add jupyterhub https://hub.jupyter.org/helm-chart/
+helm repo update
+
+## use your name for install name and namespace name
+helm upgrade --cleanup-on-fail \
+  --install juypterhelm jupyterhub/jupyterhub \
+  --namespace espm-157 \
+  --create-namespace \
+  --version=3.3.7 \
+  --timeout 90m0s \
+  --values values.yaml
+  --values secrets.yaml
+
