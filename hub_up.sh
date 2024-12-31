717bdbbb41a6f4c079d5bcc3353ec71224172174
Carl Boettiger <cboettig@gmail.com>
2024-12-31 15:53:11 -0800

    update

--- hub_up.sh
+++ hub_up.sh
@@ -5,11 +5,14 @@ helm repo update
 
 ## use your name for install name and namespace name
 helm upgrade --cleanup-on-fail \
-  --install juypterhelm jupyterhub/jupyterhub \
+  --install jupyterhelm jupyterhub/jupyterhub \
   --namespace espm-157 \
   --create-namespace \
   --version=3.3.7 \
   --timeout 90m0s \
   --values values.yaml \
   --values secrets.yaml
+
+# purge everything related to helm
+# helm delete jupyterhelm 
 
