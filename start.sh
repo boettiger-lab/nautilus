c88e7c9a087cd44c0a944153e47b9f4c1a56594c
Carl Boettiger <cboettig@gmail.com>
2025-02-25 09:55:03 -0800

    permissions

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # needs gettext to use envsubst
-# should already be installed -- no root in postStart hook
 
-mkdir -p $HOME/.continue; (command -v envsubst && envsubst < /opt/share/continue/config.json > $HOME/.continue/config.json) &>/dev/null
+
+if [ ! -f "$HOME/.continue/config.json" ];
+then 
+	mkdir -p $HOME/.continue
+  (command -v envsubst && envsubst < /opt/share/continue/config.json > $HOME/.continue/config.json) &>/dev/null
+	chown -R ${NB_USER} .continue/config.json
+fi
 
 
